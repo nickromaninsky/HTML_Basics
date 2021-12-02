a8809aeb8e30c449b56007363bddc46fb9bd8756
Amrix <nickromo28@gmail.com>
2021-12-02 00:07:01 -0500

    changes

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-//this is my first javascript code
-console.log('Hello World');
+
+let name = 'Mosh';
+console.log(name);
+
+// cannot be a reserved keyword
+// should be meaningful
+// cannot start with a number (1name)
+//cannot contain space or hyphen (-)
+// are case-sensitive
+
+let firstName = Nick
+let lastName = Romaninsky
